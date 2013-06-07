464302fb92c9aee8fb5fba0845800072fccb9d37
xav <xlembo@eliberty.fr>
2013-06-08 00:54:02 +0200

    fix

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -37,13 +37,6 @@ class php5::nginx {
     require => Package['nginx-full']
   }
 
-  file { '/home/log' :
-      ensure => directory,
-      owner => www-data,
-      group => root,
-      mode => 755
-  }
-
   file { '/home/log/nginx' :
       ensure => directory,
       owner => www-data,
